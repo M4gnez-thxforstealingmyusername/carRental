62f7f3ac5db51881cf5b6d1c8e961872dd5d1a8d
Krz3ch <89007379+Krz3ch@users.noreply.github.com>
2024-06-02 19:45:55 +0200

    Styled Home and styled rest of components

--- src/app/rent-form/rent-form.component.ts
+++ src/app/rent-form/rent-form.component.ts
@@ -10,21 +10,23 @@ import { Rent } from '../Rent';
 import { RentService } from '../rent.service';
 import { asapScheduler, map } from 'rxjs';
 import { sanitizeIdentifier } from '@angular/compiler';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-rent-form',
   standalone: true,
   imports: [FormsModule, CommonModule],
   templateUrl: './rent-form.component.html',
-  styleUrl: './rent-form.component.css'
+  styleUrl: './rent-form.component.css',
 })
-export class RentFormComponent implements OnInit{
+export class RentFormComponent implements OnInit {
+  router = inject(Router);
   userService = inject(UserService);
   carService = inject(CarService);
   rentService = inject(RentService);
   route = inject(ActivatedRoute);
 
-  carId! : string | null;
+  carId!: string | null;
   user!: User | null;
   car!: Car | null;
 
@@ -36,61 +38,68 @@ export class RentFormComponent implements OnInit{
   occupiedDates: Date[] = [];
   dateCorrect = true;
 
-  async ngOnInit(): Promise<void>{
+  async ngOnInit(): Promise<void> {
     this.carId = this.route.snapshot.paramMap.get('id');
 
-    this.userService.getUser().subscribe(async data => {
-      this.user = data
-      if(this.user.email == undefined)
-        this.user = null;
-    })
+    this.userService.getUser().subscribe(async (data) => {
+      this.user = data;
+      if (this.user.email == undefined) this.user = null;
+    });
 
-    this.carService.getCars().subscribe(data => {
-      this.car = data.find(x => x.id == parseInt(this.carId ?? "")) ?? null
-    })
+    this.carService.getCars().subscribe((data) => {
+      this.car = data.find((x) => x.id == parseInt(this.carId ?? '')) ?? null;
+    });
 
     this.getRents();
   }
 
-  getRents(){
-    this.rentService.getRent().subscribe(data => {
-      this.rents = data.filter(x => x.carId == parseInt(this.carId ?? ""))
-      this.myRents = this.rents.filter(x => x.userId == this.user?.id);
-
+  getRents() {
+    this.rentService.getRent().subscribe((data) => {
+      this.rents = data.filter((x) => x.carId == parseInt(this.carId ?? ''));
+      this.myRents = this.rents.filter((x) => x.userId == this.user?.id);
     });
   }
 
-
-  checkDate(){
-    return this.rentStartDate < this.rentEndDate
+  checkDate() {
+    return this.rentStartDate < this.rentEndDate;
   }
 
-  testDates(){
-    this.rents.filter(x => x.carId == parseInt(this.carId ?? "")).forEach(x =>{
-      this.occupiedDates.push(...this.dateSpan(x.startDate, x.endDate))
-    })
+  testDates() {
+    this.rents
+      .filter((x) => x.carId == parseInt(this.carId ?? ''))
+      .forEach((x) => {
+        this.occupiedDates.push(...this.dateSpan(x.startDate, x.endDate));
+      });
 
     this.dateCorrect = true;
 
-    this.dateSpan(this.rentStartDate, this.rentEndDate).forEach(item => {
-      if(this.occupiedDates.some(date => date.getDate() == item.getDate())){
+    this.dateSpan(this.rentStartDate, this.rentEndDate).forEach((item) => {
+      if (this.occupiedDates.some((date) => date.getDate() == item.getDate())) {
         this.dateCorrect = false;
       }
-    })
+    });
 
-    if(this.dateCorrect){
-      this.rentService.addRent(new Rent(parseInt(this.carId ?? ""), this.rentStartDate, this.rentEndDate)).subscribe();
-      
+    if (this.dateCorrect) {
+      this.rentService
+        .addRent(
+          new Rent(
+            parseInt(this.carId ?? ''),
+            this.rentStartDate,
+            this.rentEndDate
+          )
+        )
+        .subscribe();
+      this.router.navigate(['/user']);
     }
   }
 
-  dateSpan(startDateString : string, endDateString : string) : Date[]{
+  dateSpan(startDateString: string, endDateString: string): Date[] {
     var sd = new Date(startDateString);
     var ed = new Date(endDateString);
 
-    var ds : Date[]= [];
+    var ds: Date[] = [];
 
-    for(var i = sd; i <= ed; i.setDate(i.getDate() + 1)){
+    for (var i = sd; i <= ed; i.setDate(i.getDate() + 1)) {
       var nd = new Date();
       nd.setDate(i.getDate());
       ds.push(nd);
